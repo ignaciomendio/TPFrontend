ef29d6601c5eb7fd709a706c54f2b1bc0b2f68e3
imendiola <ignaciomendio@gmail.com>
2022-07-27 18:59:06 -0300

    Completado el formulario de login, la parte estetica y de validaciones, ruteo completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.form = formBuilder.group({
+      email:['', [Validators.required, Validators.email]],
+      password:['', [Validators.required, Validators.minLength(8)]],
+    })
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  get email(){
+    return this.form.get('email');
+  }
+
+  get password(){
+    return this.form.get('password');
+  }
+
+
+}
